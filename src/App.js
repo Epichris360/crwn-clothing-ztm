06fedf66357731c5ca6513b481854b3443282bed
cristian <segoviacristian23@hotmail.com>
2020-12-25 21:39:04 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import HomePage from './HomePage.component'
+
+function App() {
+  return <div>
+    <HomePage />
+  </div>
+}
+export default App;
+// video 65.. this is awesome
