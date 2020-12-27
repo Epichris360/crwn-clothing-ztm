29be7576247dfc62674caaf1e663bef0733b940d
cristian <segoviacristian23@hotmail.com>
2020-12-27 18:52:23 -0300

    broke homepage into seperate components, styling done for menu items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+
+import Directory from '../../components/directory/directory.component'
+
+import './HomePage.styles.scss'
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+);
+
+export default HomePage;
